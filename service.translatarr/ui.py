a8f2198ca4a044390eecfac81fbf75b25edb41bc
Addonniss <bogdan.matara@gmail.com>
2026-03-03 08:51:00 +0200

    Update ui.py

--- service.translatarr/ui.py
+++ service.translatarr/ui.py
@@ -99,7 +99,7 @@ class TranslationProgress:
         self.start_time = time.time()
 
         # Milestones
-        self.milestones = {10, 25, 50, 75, 90, 100}
+        self.milestones = {5, 15, 35, 60, 85}
         self.triggered = set()
 
         self.pDialog = None
