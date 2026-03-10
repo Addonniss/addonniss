3567e8c9001abc319add890461b860da64c3fd13
Addonniss <bogdan.matara@gmail.com>
2026-03-11 01:33:38 +0200

    Update launcher.py

--- script.kodiarr.instant/launcher.py
+++ script.kodiarr.instant/launcher.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+import sys
 import xbmcaddon
 
+def _has_action():
+    argv_text = " ".join([str(x) for x in sys.argv]).lower()
+    return "action=" in argv_text
+
 if __name__ == "__main__":
-    xbmcaddon.Addon().openSettings()
+    if _has_action():
+        from resources.lib.router import run
+        run()
+    else:
+        xbmcaddon.Addon("script.kodiarr.instant").openSettings()
