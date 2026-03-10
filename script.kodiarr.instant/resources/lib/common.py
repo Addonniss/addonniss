5870b8283aac2c851e7529ad2344ba9d6986ac37
Addonniss <bogdan.matara@gmail.com>
2026-03-10 23:56:58 +0200

    Update common.py

--- script.kodiarr.instant/resources/lib/common.py
+++ script.kodiarr.instant/resources/lib/common.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import xbmc
 import xbmcgui
 import xbmcaddon
@@ -6,23 +7,34 @@ ADDON = xbmcaddon.Addon()
 
 
 def log(msg, level=xbmc.LOGINFO):
-    xbmc.log("[KodiARR] " + str(msg), level)
+    xbmc.log("[KodiARR Instant] {}".format(msg), level)
 
 
 def notify(title, message, icon=xbmcgui.NOTIFICATION_INFO):
     xbmcgui.Dialog().notification(title, message, icon, 5000)
 
 
-def get_setting(key):
-    return ADDON.getSetting(key)
+def get_setting(key, default=""):
+    try:
+        value = ADDON.getSetting(key)
+        return value if value != "" else default
+    except Exception:
+        return default
 
 
 def get_int(key, default=0):
     try:
-        return int(ADDON.getSetting(key))
-    except:
+        return int(get_setting(key, str(default)))
+    except Exception:
         return default
 
 
 def clean_url(url):
-    return url.rstrip("/")
+    return (url or "").strip().rstrip("/")
+
+
+def open_settings():
+    try:
+        ADDON.openSettings()
+    except Exception:
+        pass
