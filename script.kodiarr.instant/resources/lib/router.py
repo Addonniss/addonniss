e83a1aa1a29b56e38be944d02a3fee4a956c6895
Addonniss <bogdan.matara@gmail.com>
2026-03-10 23:44:07 +0200

    Update router.py

--- script.kodiarr.instant/resources/lib/router.py
+++ script.kodiarr.instant/resources/lib/router.py
@@ -1,29 +1,23 @@
-# -*- coding: utf-8 -*-
 import xbmc
-
-from .common import notify, log
-from . import radarr
-from . import sonarr
+from . import radarr, sonarr
+from .common import log, notify
 
 
 def run():
+
     db_type = xbmc.getInfoLabel("ListItem.DBTYPE").lower()
     item_type = xbmc.getInfoLabel("ListItem.Property(item.type)").lower()
 
-    log("DBTYPE='{}' item.type='{}'".format(db_type, item_type))
+    log(f"DBTYPE={db_type} item.type={item_type}")
 
-    movie_types = ("movie",)
-    tv_types = ("tvshow", "season", "episode")
-
-    if db_type in movie_types or item_type in movie_types:
+    if db_type == "movie" or item_type == "movie":
         log("Routing to Radarr")
         radarr.run()
         return
 
-    if db_type in tv_types or item_type in tv_types:
+    if db_type in ["tvshow", "season", "episode"] or item_type in ["tvshow", "season", "episode"]:
         log("Routing to Sonarr")
         sonarr.run()
         return
 
-    notify("KodiARR Instant", "Unsupported item type.")
-    log("Unsupported item type. DBTYPE='{}' item.type='{}'".format(db_type, item_type), xbmc.LOGERROR)
+    notify("KodiARR Instant", "Unsupported item type")
